8efc6c8e7bac43495b9b6e9d39615e3164a0e930
rolandvital2007 <rolandvital2007@gmail.com>
2020-05-06 23:07:36 -0400

    first commit

---
+++ questions.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+        title: "Commonly used data types Do Not includ_______.",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+
+    
+{
+    title: "The condition in if/else statement is enclodes within_______.",
+    choices: ["quotes", "curly brackets", "parentheses", "square bracketes"],
+    answer: "parentheses"
+},
+
+    {
+        title: "Arrays in JavaScript can be used to store_______.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+
+    {
+        title: "String valuesn must be enclosed within_______when being assigned to variable.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+
+    {
+        title: "A very useful tools used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+
+];
